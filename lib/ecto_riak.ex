284d7910646d23fd2a6ff42c435b0ab01727c3d5
Yew-wei Tan <yewwei.tan@gmail.com>
2014-03-01 10:43:47 +1100

    remove useless application behaviour and useless supervisor

--- lib/ecto_riak.ex
+++ lib/ecto_riak.ex
@@ -1,9 +1,3 @@
 defmodule EctoRiak do
-  use Application.Behaviour
-
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    EctoRiak.Supervisor.start_link
-  end
+  ## placeholder
 end
